57dba2e865082b7fcd5ca336ad16e5dcc4bde4de
George Zimvragos <gzimvragos@gmail.com>
2026-01-20 14:31:54 +0200

    fixed non-working blankline

--- lua/plugins/blankline.lua
+++ lua/plugins/blankline.lua
@@ -1,26 +1,42 @@
 return {
-    "lukas-reineke/indent-blankline.nvim",
-    event = "User FilePost",
-    config = function()
-        local hooks = require("ibl.hooks")
+  "lukas-reineke/indent-blankline.nvim",
+  event = { "BufReadPost", "BufNewFile" },
+  config = function()
+    local ibl = require("ibl")
+    local hooks = require("ibl.hooks")
 
-        -- Hide first space indent level
-        hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+    -- Hide first indent level
+    hooks.register(
+      hooks.type.WHITESPACE,
+      hooks.builtin.hide_first_space_indent_level
+    )
 
-        require("ibl").setup({
-            indent = {
-                char = "│",
-                highlight = "IblChar"
-            },
-            scope = {
-                char = "│",
-                highlight = "IblScopeChar"
-            },
-        })
+    ibl.setup({
+      indent = {
+        char = "│",
+        highlight = "IblIndent",
+      },
+      scope = {
+        enabled = true,
+        char = "│",
+        highlight = "IblScope",
+        show_start = false,
+        show_end = false,
+      },
+      exclude = {
+        buftypes = { "terminal", "nofile", "quickfix", "prompt" },
+        filetypes = {
+          "help",
+          "lazy",
+          "mason",
+          "NvimTree",
+          "neo-tree",
+          "TelescopePrompt",
+        },
+      },
+    })
 
-        -- Load base46 blankline highlights again (ensures proper coloring)
-        pcall(function()
-            dofile(vim.g.base46_cache .. "blankline")
-        end)
-    end,
+    vim.api.nvim_set_hl(0, "IblIndent", { fg = "#3b3b3b" })
+    vim.api.nvim_set_hl(0, "IblScope",  { fg = "#6f6f6f", bold = true })
+  end,
 }
