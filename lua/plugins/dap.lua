6c29ce1960061a992147159c62302e7dcf9f550b
George Zimvragos <gzimvragos@gmail.com>
2026-01-29 17:03:46 +0200

    added dap template for future development

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,277 @@
+return {
+    -- "mfussenegger/nvim-dap",
+    -- dependencies = {
+    --     -- UI plugins for better debugging experience
+    --     "rcarriga/nvim-dap-ui",
+    --     "nvim-neotest/nvim-nio",
+    --     "theHamsta/nvim-dap-virtual-text",
+    --
+    --     -- Language-specific adapters
+    --     "leoluz/nvim-dap-go",
+    --     "mfussenegger/nvim-dap-python",
+    -- },
+    -- event = "VeryLazy",
+    -- config = function()
+    --     local dap = require "dap"
+    --     local dapui = require "dapui"
+    --
+    --     -- ============================================
+    --     -- DAP UI Setup
+    --     -- ============================================
+    --     dapui.setup {
+    --         icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
+    --         mappings = {
+    --             expand = { "<CR>", "<2-LeftMouse>" },
+    --             open = "o",
+    --             remove = "d",
+    --             edit = "e",
+    --             repl = "r",
+    --             toggle = "t",
+    --         },
+    --         layouts = {
+    --             {
+    --                 elements = {
+    --                     { id = "scopes", size = 0.25 },
+    --                     { id = "breakpoints", size = 0.25 },
+    --                     { id = "stacks", size = 0.25 },
+    --                     { id = "watches", size = 0.25 },
+    --                 },
+    --                 size = 40,
+    --                 position = "left",
+    --             },
+    --             {
+    --                 elements = {
+    --                     { id = "repl", size = 0.5 },
+    --                     { id = "console", size = 0.5 },
+    --                 },
+    --                 size = 10,
+    --                 position = "bottom",
+    --             },
+    --         },
+    --         floating = {
+    --             max_height = nil,
+    --             max_width = nil,
+    --             border = "rounded",
+    --             mappings = {
+    --                 close = { "q", "<Esc>" },
+    --             },
+    --         },
+    --     }
+    --
+    --     -- ============================================
+    --     -- Virtual Text Setup
+    --     -- ============================================
+    --     require("nvim-dap-virtual-text").setup {
+    --         enabled = true,
+    --         enabled_commands = true,
+    --         highlight_changed_variables = true,
+    --         highlight_new_as_changed = false,
+    --         show_stop_reason = true,
+    --         commented = false,
+    --         only_first_definition = true,
+    --         all_references = false,
+    --         filter_references_pattern = "<module>",
+    --         virt_text_pos = "eol",
+    --         all_frames = false,
+    --         virt_lines = false,
+    --         virt_text_win_col = nil,
+    --     }
+    --
+    --     -- ============================================
+    --     -- Auto-open/close UI
+    --     -- ============================================
+    --     dap.listeners.before.attach.dapui_config = function()
+    --         dapui.open()
+    --     end
+    --     dap.listeners.before.launch.dapui_config = function()
+    --         dapui.open()
+    --     end
+    --     dap.listeners.before.event_terminated.dapui_config = function()
+    --         dapui.close()
+    --     end
+    --     dap.listeners.before.event_exited.dapui_config = function()
+    --         dapui.close()
+    --     end
+    --
+    --     -- ============================================
+    --     -- Signs/Icons
+    --     -- ============================================
+    --     vim.fn.sign_define("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = "" })
+    --     vim.fn.sign_define(
+    --         "DapBreakpointCondition",
+    --         { text = "◆", texthl = "DapBreakpointCondition", linehl = "", numhl = "" }
+    --     )
+    --     vim.fn.sign_define("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = "" })
+    --     vim.fn.sign_define("DapStopped", { text = "→", texthl = "DapStopped", linehl = "DapStoppedLine", numhl = "" })
+    --     vim.fn.sign_define(
+    --         "DapBreakpointRejected",
+    --         { text = "○", texthl = "DapBreakpointRejected", linehl = "", numhl = "" }
+    --     )
+    --
+    --     -- ============================================
+    --     -- Python Configuration
+    --     -- ============================================
+    --     require("dap-python").setup "python" -- or specify path to python with debugpy installed
+    --     -- Example: require("dap-python").setup("~/.virtualenvs/debugpy/bin/python")
+    --
+    --     table.insert(dap.configurations.python, {
+    --         type = "python",
+    --         request = "launch",
+    --         name = "Launch file with arguments",
+    --         program = "${file}",
+    --         args = function()
+    --             local args_string = vim.fn.input "Arguments: "
+    --             return vim.split(args_string, " +")
+    --         end,
+    --     })
+    --
+    --     -- ============================================
+    --     -- Go Configuration
+    --     -- ============================================
+    --     require("dap-go").setup {
+    --         dap_configurations = {
+    --             {
+    --                 type = "go",
+    --                 name = "Attach remote",
+    --                 mode = "remote",
+    --                 request = "attach",
+    --             },
+    --         },
+    --         delve = {
+    --             path = "dlv",
+    --             initialize_timeout_sec = 20,
+    --             port = "${port}",
+    --             args = {},
+    --             build_flags = "",
+    --         },
+    --     }
+    --
+    --     -- ============================================
+    --     -- JavaScript/TypeScript Configuration (using node-debug2)
+    --     -- ============================================
+    --     dap.adapters.node2 = {
+    --         type = "executable",
+    --         command = "node",
+    --         args = { vim.fn.stdpath "data" .. "/mason/packages/node-debug2-adapter/out/src/nodeDebug.js" },
+    --     }
+    --
+    --     dap.configurations.javascript = {
+    --         {
+    --             name = "Launch",
+    --             type = "node2",
+    --             request = "launch",
+    --             program = "${file}",
+    --             cwd = vim.fn.getcwd(),
+    --             sourceMaps = true,
+    --             protocol = "inspector",
+    --             console = "integratedTerminal",
+    --         },
+    --         {
+    --             name = "Attach to process",
+    --             type = "node2",
+    --             request = "attach",
+    --             processId = require("dap.utils").pick_process,
+    --         },
+    --     }
+    --
+    --     dap.configurations.typescript = dap.configurations.javascript
+    --
+    --     -- ============================================
+    --     -- C/C++/Rust Configuration (using lldb or gdb)
+    --     -- ============================================
+    --     -- Using lldb (macOS/Linux)
+    --     dap.adapters.lldb = {
+    --         type = "executable",
+    --         command = "/usr/bin/lldb-vscode", -- adjust path as needed
+    --         name = "lldb",
+    --     }
+    --
+    --     -- Using gdb (Linux)
+    --     dap.adapters.gdb = {
+    --         type = "executable",
+    --         command = "gdb",
+    --         args = { "-i", "dap" },
+    --     }
+    --
+    --     dap.configurations.cpp = {
+    --         {
+    --             name = "Launch",
+    --             type = "lldb", -- or "gdb"
+    --             request = "launch",
+    --             program = function()
+    --                 return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+    --             end,
+    --             cwd = "${workspaceFolder}",
+    --             stopOnEntry = false,
+    --             args = {},
+    --             runInTerminal = false,
+    --         },
+    --         {
+    --             name = "Attach to process",
+    --             type = "lldb",
+    --             request = "attach",
+    --             pid = require("dap.utils").pick_process,
+    --             args = {},
+    --         },
+    --     }
+    --
+    --     dap.configurations.c = dap.configurations.cpp
+    --     dap.configurations.rust = dap.configurations.cpp
+    --
+    --     -- ============================================
+    --     -- Key Mappings
+    --     -- ============================================
+    --     --[[
+    --
+    --     local map = vim.keymap.set
+    --
+    --     -- Basic debugging
+    --     map("n", "<F5>", dap.continue, { desc = "DAP: Continue" })
+    --     map("n", "<F10>", dap.step_over, { desc = "DAP: Step Over" })
+    --     map("n", "<F11>", dap.step_into, { desc = "DAP: Step Into" })
+    --     map("n", "<F12>", dap.step_out, { desc = "DAP: Step Out" })
+    --
+    --     -- Breakpoints
+    --     map("n", "<leader>db", dap.toggle_breakpoint, { desc = "DAP: Toggle Breakpoint" })
+    --     map("n", "<leader>dB", function()
+    --         dap.set_breakpoint(vim.fn.input "Breakpoint condition: ")
+    --     end, { desc = "DAP: Conditional Breakpoint" })
+    --     map("n", "<leader>dlp", function()
+    --         dap.set_breakpoint(nil, nil, vim.fn.input "Log point message: ")
+    --     end, { desc = "DAP: Log Point" })
+    --
+    --     -- UI controls
+    --     map("n", "<leader>du", dapui.toggle, { desc = "DAP: Toggle UI" })
+    --     map("n", "<leader>dr", dap.repl.toggle, { desc = "DAP: Toggle REPL" })
+    --     map("n", "<leader>dl", dap.run_last, { desc = "DAP: Run Last" })
+    --
+    --     -- Inspection
+    --     map({ "n", "v" }, "<leader>dh", require("dap.ui.widgets").hover, { desc = "DAP: Hover" })
+    --     map({ "n", "v" }, "<leader>dp", require("dap.ui.widgets").preview, { desc = "DAP: Preview" })
+    --     map("n", "<leader>df", function()
+    --         local widgets = require "dap.ui.widgets"
+    --         widgets.centered_float(widgets.frames)
+    --     end, { desc = "DAP: Frames" })
+    --     map("n", "<leader>ds", function()
+    --         local widgets = require "dap.ui.widgets"
+    --         widgets.centered_float(widgets.scopes)
+    --     end, { desc = "DAP: Scopes" })
+    --
+    --     -- Session management
+    --     map("n", "<leader>dc", dap.terminate, { desc = "DAP: Terminate" })
+    --     map("n", "<leader>dC", function()
+    --         dap.clear_breakpoints()
+    --         vim.notify("All breakpoints cleared", vim.log.levels.INFO)
+    --     end, { desc = "DAP: Clear All Breakpoints" })
+    --
+    --     -- Language-specific
+    --     map("n", "<leader>dgt", function()
+    --         require("dap-go").debug_test()
+    --     end, { desc = "DAP: Go Debug Test" })
+    --     map("n", "<leader>dgl", function()
+    --         require("dap-go").debug_last_test()
+    --     end, { desc = "DAP: Go Debug Last Test" })
+    --
+    --     ]]
+    -- end,
+}
